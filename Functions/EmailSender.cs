61bebcaf05d2d467212bf7b947dc55ba83aef074
Farangis Ölund <bless.faro@gmail.com>
2024-05-07 15:28:22 +0200

    EmailSender is completed and tested

---
+++ Functions/EmailSender.cs
@@ -0,0 +1,38 @@
+
+using Azure.Messaging.ServiceBus;
+using EmailProvider.Services;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+
+namespace EmailProvider.Functions;
+
+public class EmailSender(ILogger<EmailSender> logger, IEmailService emailService)
+{
+    private readonly ILogger<EmailSender> _logger = logger;
+    private readonly IEmailService _emailService = emailService;
+
+    [Function(nameof(EmailSender))]
+    public async Task Run([ServiceBusTrigger("email_request", Connection = "ServiceBusConnection")]
+                           ServiceBusReceivedMessage message,
+                           ServiceBusMessageActions messageActions)
+    {
+        try
+        {
+            var emailRequest = _emailService.UnpackEmailRequest(message);
+            if (emailRequest != null && !string.IsNullOrEmpty(emailRequest.To))
+            {
+                if (_emailService.SendEmail(emailRequest))
+                {
+                    await messageActions.CompleteMessageAsync(message);
+                }
+            }
+        } 
+        catch (Exception ex)
+        {
+            _logger.LogError($": EmailSender.Run :: {ex.Message}");
+        }
+    }
+
+   
+}
